91adb7eeccb24c783418d9aafff7933b7627bf84
shaogaige <shaogaige@126.com>
2020-07-21 18:14:44 +0800

    Add files via upload

--- src/com/idata/core/DataBaseHandle.java
+++ src/com/idata/core/DataBaseHandle.java
@@ -222,6 +222,159 @@ public class DataBaseHandle extends DataBase {
 			}
 		}
 	}
+	
+	public boolean exePreparedSQLUpdate(String table, SuperObject data,String idField,String geometryField) {
+		ConnectionObject conn = null;
+		PreparedStatement preStmt = null;
+		try 
+		{
+			String sql = "update "+table+" set ";
+			PreparedParam preparedParam = new PreparedParam();
+			int cur = 1;
+			//获取所有列名称,应该按上传的字段
+			for(String key:data.getKeys())
+			{
+				String columnName = key;
+				
+				Value v = data.getProperty(columnName);
+				if(v == null)
+				{
+					continue;
+				}
+				if(columnName.equalsIgnoreCase(idField))
+				{
+					continue;
+				}
+				//空间字段的处理
+				else if(columnName.equalsIgnoreCase(geometryField))
+				{
+					sql += columnName+"";
+					sql += "=ST_GeomFromText(?, 4490),";
+				}
+				else
+				{
+					sql += columnName+"=?,";
+				}
+				
+				preparedParam.addParam(cur, data.getProperty(columnName));
+				cur++;
+			}
+			sql = sql.substring(0, sql.length()-1);
+			
+			if(data.getProperty(idField).isStringValue())
+			{
+				sql += "where id='"+data.getProperty(idField).getString_value()+"'";
+			}
+			else if(data.getProperty(idField).isLongValue())
+			{
+				sql += "where id="+data.getProperty(idField).getLong_value();
+			}
+			else
+			{
+				sql += "where id="+data.getProperty(idField).getInt_value();
+			}
+			conn = ConnectionManager.borrowConnectionObject(connInfo);
+			preStmt = conn.getConnection().prepareStatement(sql);
+			SQLPreparedParamUtil.setSQLPreparedParam(preStmt, preparedParam);
+			preStmt.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			DBCException.logException(DBCException.E_SQL, e);
+			return false;
+		}
+		finally
+		{
+			if(preStmt != null)
+			{
+				try 
+				{
+					preStmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(conn != null)
+			{
+				ConnectionManager.returnConnectionObject(conn);
+			}
+		}
+	}
+	
+	
+	public int exePreparedSQLInsert(String table, SuperObject data,String geometryField) {
+		// TODO Auto-generated method stub
+		ConnectionObject conn = null;
+		PreparedStatement preStmt = null;
+		ResultSet rs = null;
+		try 
+		{
+			String sql = "insert into "+table+ "(";
+			String values = "(";
+			PreparedParam preparedParam = new PreparedParam();
+			int index = 1;
+			//获取所有列名称
+			for(String key:data.getKeys())
+			{
+				String columnName = key;
+				Value v = data.getProperty(key);
+				if(v == null)
+				{
+					continue;
+				}
+				//空间字段的处理
+				if(columnName.equalsIgnoreCase(geometryField))
+				{
+					sql += columnName+",";
+					values += "ST_GeomFromText(?, 4490),";
+				}
+				else
+				{
+					sql += columnName+",";
+					values += "?,";
+				}
+				preparedParam.addParam(index, v);
+				index++;
+			}
+			sql = sql.substring(0, sql.length()-1)+") values";
+			values = values.substring(0, values.length()-1)+")";
+			sql = sql+values;
+			conn = ConnectionManager.borrowConnectionObject(connInfo);
+			preStmt = conn.getConnection().prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+			SQLPreparedParamUtil.setSQLPreparedParam(preStmt, preparedParam);
+			int f = preStmt.executeUpdate();
+			//rs = preStmt.getGeneratedKeys();
+			return f;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			DBCException.logException(DBCException.E_SQL, e);
+			return 0;
+		}
+		finally
+		{
+			try 
+			{
+				if(rs != null)
+				{
+					rs.close();
+				}
+				if(preStmt != null)
+				{
+					preStmt.close();
+				}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(conn != null)
+			{
+				ConnectionManager.returnConnectionObject(conn);
+			}
+		}
+	}
 
 	public List<SuperObject> getMetaData2(String sql) {
 		// TODO Auto-generated method stub
